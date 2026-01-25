dac0b1a89ee5c3e8dd7b20ee3db8e9d20b8aa9b9
yerassyl <moldabekulyerasyl@gmail.com>
2026-01-25 19:32:07 +0500

    Add python Data_Types examples

---
+++ Practice1/python-basics/Data_types.py
@@ -0,0 +1,29 @@
+#1
+l = ["apple", "banana", "orange"]
+print(type(l))
+print("-------------------------------")
+
+#2
+x = 89.5
+print(type(x))
+print("-------------------------------")
+
+#3
+student = {"Name" : "Yerassyl", "Age" : 18}
+print(student["Name"], student["Age"])
+print("-------------------------------")
+
+#4
+x = 10
+y = True
+if x > 9:
+    print(y)
+else:
+    y = False
+    print(y)
+
+print("-------------------------------")
+
+#5
+t = "Hello"
+print(type(t))
