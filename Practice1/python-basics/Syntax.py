2d6b4d6d3be922fadea9a7fc18426445ca60aa96
yerassyl <moldabekulyerasyl@gmail.com>
2026-01-25 16:41:20 +0500

    Add simple Python syntax examples

---
+++ Practice1/python-basics/Syntax.py
@@ -0,0 +1,22 @@
+#1
+if 7 > 5:
+    print("Seven is greater than five!")
+
+#2
+if 2 < 3:
+    print("Two is less than three!")
+
+#3
+if 112 > 110:
+    print("U are accepted to the KBTU!")
+
+#4
+if 5 > 4:
+    print("1")
+    print("Five is greater than four!")
+
+#5
+if 1 < 6:
+    print("Six is greater than one!")
+    print("One is less than six!")
+    print("2")
