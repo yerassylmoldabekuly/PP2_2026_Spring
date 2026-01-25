aac2d562c687a27071d33a53710406b6009f87a4
yerassyl <moldabekulyerasyl@gmail.com>
2026-01-25 16:59:07 +0500

    Add python basic Comment examples

---
+++ Practice1/python-basics/Comments.py
@@ -0,0 +1,23 @@
+#1
+#This is a comment
+print("Above the line is a comment")
+
+#2
+#It is used for clarity
+
+#3
+print("Hello, World!") #This is a comment
+
+#4
+#Comment is a useful tool
+#that can be written on
+#more than one line
+
+#5
+# another way of writing it:
+# using three strings as follows:
+"""
+like this
+8+5
+PP2 is great!
+"""
