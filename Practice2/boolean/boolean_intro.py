2fe4c6f73d54a72ddc11482c26bfae49f368c3b8
yerassyl <moldabekulyerasyl@gmail.com>
2026-01-31 20:31:10 +0500

    Add Practice2 - Python control flow examples boolean

---
+++ Practice2/boolean/boolean_intro.py
@@ -0,0 +1,31 @@
+#1
+print(10 > 9)
+
+print("----------------------------")
+
+#2
+print(10 != 9)
+
+print("----------------------------")
+
+#3
+print(10 < 8)
+
+print("----------------------------")
+
+#4
+print(100 == 1001)
+
+print("----------------------------")
+
+#5
+
+print(bool(False)) #some values are false
+print(bool(None))
+print(bool(0))
+print(bool(""))
+print(bool())
+print(bool([]))
+print(bool({}))
+
+
